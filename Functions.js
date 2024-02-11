14d18bd664e7844cd685380775570acf37cbded7
Jeovani <jeovaniquintanilla35@gmail.com>
2024-02-11 17:41:47 -0500

    Defined login function

--- Functions.js
+++ Functions.js
@@ -1,8 +1,17 @@
-let defaultUser =  'swagger';
-let defaultPassword = 12345;
+function login(){
+    let defaultUser =  "admin";
+    let defaultPassword = "123";
 
-function login(user,password){
+    let user = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+    //console.log(user + password);
+    //console.log(defaultUser + defaultPassword);
 
+    if(user == defaultUser && password == defaultPassword){
+        location.href="Parking-Pal.html";
+    }else {
+        alert("Incorrect username or password");
+    }
 }
 
 
