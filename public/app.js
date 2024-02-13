a1deec677a925873e30a797cd08be127e1b591f1
George N. - PC <91430969+Gevoooo@users.noreply.github.com>
2024-02-12 22:37:59 -0500

    feat: Login with google popup

---
+++ public/app.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", event => {
+    const app = firebase.app();
+});
+  
+function googleLogin() {
+const provider = new firebase.auth.GoogleAuthProvider();
+firebase.auth().signInWithPopup(provider).then(result => {
+    const user = result.user;
+    console.log(user)
+})
+.catch(console.log)
+}
