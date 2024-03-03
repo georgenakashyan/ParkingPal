dcfe665a0424518dea6dfbfae23f86f2256c8f9a
George N. - PC <91430969+Gevoooo@users.noreply.github.com>
2024-03-03 18:09:29 -0500

    feat: add custom colors to match logo

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./public/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'PP-dark-blue': '#0b1e32',
+        'PP-blue': '#1e3a5b',
+        'PP-orange': '#db741f',
+        'PP-light-orange': '#ebab59',
+      },
+    },
   },
   plugins: [],
 }
