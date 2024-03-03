2a25b524bfa6e3058346f56425d946efd4354c6d
George N. - PC <91430969+Gevoooo@users.noreply.github.com>
2024-03-03 01:35:19 -0500

    docs: fix indenting

--- public/app.js
+++ public/app.js
@@ -98,42 +98,42 @@ function makeAccountEmailAndPassword(email, firstName, lastName, password, retyp
         errorField.innerHTML = "Your passwords do not match";
     } else {
         firebase.auth().createUserWithEmailAndPassword(email, password)
-            .then((userCredential) => {
-                const user = userCredential.user;
-                console.log("Auth Account successfully created");
-                createAccountDocument(user, email, firstName, lastName);
-                errorField.innerHTML = "";
-                mainPage();
-            })
-            .catch((error) => {
-                switch (error.code) {
-                    case 'auth/missing-email':
-                        errorField.innerHTML = "Enter your email.";
-                        break;
-                    case 'auth/missing-password':
-                        errorField.innerHTML = "Enter your password.";
-                        break;
-                    case 'auth/email-already-in-use':
-                        errorField.innerHTML = "Email address is already in use.";
-                        break;
-                    case 'auth/invalid-credential':
-                        errorField.innerHTML = "Email or password is incorrect.";
-                        break;
-                    case 'auth/invalid-email':
-                        errorField.innerHTML = "Email address is invalid.";
-                        break;
-                    case 'auth/operation-not-allowed':
-                        errorField.innerHTML = "Error during sign up.";
-                        break;
-                    case 'auth/weak-password':
-                        errorField.innerHTML = "Password is not strong enough. Add additional characters including special characters and numbers.";
-                        break;
-                    default:
-                        errorField.innerHTML = "";
-                        console.log(error.message);
-                        break;
-                }
-            });
+        .then((userCredential) => {
+            const user = userCredential.user;
+            console.log("Auth Account successfully created");
+            createAccountDocument(user, email, firstName, lastName);
+            errorField.innerHTML = "";
+            mainPage();
+        })
+        .catch((error) => {
+            switch (error.code) {
+                case 'auth/missing-email':
+                    errorField.innerHTML = "Enter your email.";
+                    break;
+                case 'auth/missing-password':
+                    errorField.innerHTML = "Enter your password.";
+                    break;
+                case 'auth/email-already-in-use':
+                    errorField.innerHTML = "Email address is already in use.";
+                    break;
+                case 'auth/invalid-credential':
+                    errorField.innerHTML = "Email or password is incorrect.";
+                    break;
+                case 'auth/invalid-email':
+                    errorField.innerHTML = "Email address is invalid.";
+                    break;
+                case 'auth/operation-not-allowed':
+                    errorField.innerHTML = "Error during sign up.";
+                    break;
+                case 'auth/weak-password':
+                    errorField.innerHTML = "Password is not strong enough. Add additional characters including special characters and numbers.";
+                    break;
+                default:
+                    errorField.innerHTML = "";
+                    console.log(error.message);
+                    break;
+            }
+        });
     }
 }
 
