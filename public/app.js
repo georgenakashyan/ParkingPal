63132614a2c140e78c61ecb6eaeaf1fa52210166
George N. - Macbook <91430969+Gevoooo@users.noreply.github.com>
2024-03-04 23:22:59 -0500

    feat: finish setting up user account in db method

--- public/app.js
+++ public/app.js
@@ -314,3 +314,41 @@ function fillGarages(map, garageList) {
         });
     });
 }
+
+function updateUserType(type) {
+    const user = firebase.auth().currentUser;
+    const db = firebase.firestore();
+    const accountDoc = db.collection("Account").doc(user.uid);
+    const typeDoc = db.collection(type)
+    switch (type) {
+        case "Customer":
+            typeDoc.add({
+                Accessibility: false,
+                Favorites: [],
+                Payments: [],
+                Reservations: [],
+                Reviews: [],
+                Vehicles: [],
+            }).then((doc) => {
+                accountDoc.set({
+                    Type_ID: type,
+                    Profile: "Customer/" + doc.id
+                }, { merge: true });
+            });
+            break;
+        case "Manager":
+            typeDoc.add({
+                Billing: "",
+                Garages: []
+            }).then((doc) => {
+                accountDoc.set({
+                    Type_ID: type,
+                    Profile: "Manager/" + doc.id
+                }, { merge: true });
+            });
+            break;
+        default:
+            console.log("updateUserType: Incorrect type given");
+            break;
+    }
+}
