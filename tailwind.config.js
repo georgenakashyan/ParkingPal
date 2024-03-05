e417fecc774883f57161680cd02aaf8b22df96a6
George N. - Macbook <91430969+Gevoooo@users.noreply.github.com>
2024-03-04 23:23:14 -0500

    feat: design changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,43 @@ module.exports = {
         'PP-orange': '#db741f',
         'PP-light-orange': '#ebab59',
       },
+      animation: {
+        'gradient-x':'gradient-x 15s ease infinite',
+        'gradient-y':'gradient-y 15s ease infinite',
+        'gradient-xy':'gradient-xy 15s ease infinite',
+      },
+      keyframes: {
+        'gradient-y': {
+            '0%, 100%': {
+                'background-size':'400% 400%',
+                'background-position': 'center top'
+            },
+            '50%': {
+                'background-size':'200% 200%',
+                'background-position': 'center center'
+            }
+        },
+        'gradient-x': {
+            '0%, 100%': {
+                'background-size':'200% 200%',
+                'background-position': 'left center'
+            },
+            '50%': {
+                'background-size':'200% 200%',
+                'background-position': 'right center'
+            }
+        },
+        'gradient-xy': {
+            '0%, 100%': {
+                'background-size':'400% 400%',
+                'background-position': 'left center'
+            },
+            '50%': {
+                'background-size':'200% 200%',
+                'background-position': 'right center'
+            }
+        }
+      }
     },
   },
   plugins: [],
