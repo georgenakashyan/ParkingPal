ebc3b5c769181f8d7f7064b958c56795dd47f5ee
George N. - PC <91430969+Gevoooo@users.noreply.github.com>
2024-03-05 19:41:50 -0500

    feat: create ResetPassword.js

--- public/app.js
+++ public/app.js
@@ -78,37 +78,6 @@ function logOut() {
     location.href = "index.html";
 }
 
-function resetPassword(email) {
-    var errorField = document.getElementById("notification-text");
-    const user = firebase.auth().currentUser;
-    if (user) {
-        let email = user.email;
-    }
-    firebase.auth().sendPasswordResetEmail(email)
-    .then(() => {
-        errorField.style.setProperty("color", "green");
-        errorField.innerHTML = "Sent password reset email";
-    })
-    .catch((error) => {
-        errorField.style.setProperty("color", "red");
-        switch (error.code) {
-            case 'auth/missing-email':
-                errorField.innerHTML = "Enter your email.";
-                break;
-            case 'auth/invalid-email':
-                errorField.innerHTML = "Email address is invalid.";
-                break;
-            case 'auth/operation-not-allowed':
-                errorField.innerHTML = "Error during sign up.";
-                break;
-            default:
-                errorField.innerHTML = "";
-                console.log(error.message);
-                break;
-        }
-    });
-}
-
 function mainPage() {
     return new Promise((resolve, reject) => {
         getAccountType()
