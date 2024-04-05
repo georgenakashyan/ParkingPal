b798676631c8d62b0bf141da5258ba81092820e6
George N. - Macbook <91430969+Gevoooo@users.noreply.github.com>
2024-04-05 15:32:34 -0400

    feat: add dynamic markers to map for garageList

--- public/CustomerView.js
+++ public/CustomerView.js
@@ -3,9 +3,13 @@ var areaCode = 10024;
 var map;
 var mapCenter;
 var garageList = [];
+var markers = [];
+var openInfoWindow = null;
+
 function initMap() {
+    mapCenter = new google.maps.LatLng(40.78343000, -73.96625000);
     map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 12,
+        zoom: 13,
         streetViewControl: false,
         mapTypeControl: false,
         center: {lat: userLocation[0], lng: userLocation[1]}
@@ -16,22 +20,29 @@ function initMap() {
     ];
     map.setOptions({styles: removedPOI});
     navigator.geolocation.getCurrentPosition(setCoordinates, setDefaultCoordinates);
-    google.maps.event.addListener(map, "dragend", function() {
-        var mapCenter = this.getCenter();
-        var latitude = mapCenter.lat();
-        var longitude = mapCenter.lng();
+    google.maps.event.addListener(map, "dragend", async function() {
+        mapCenter = await this.getCenter();
+        garageList = [];
+        deleteMarkers();
+        await fillGarageList();
     });
-    /* fillGarageList(); */
+    fillGarageList();
 }
 
 async function fillGarageList() {
-    await firebase.firestore().collection("Garage").where("AreaCode", "==", areaCode).get()
+    await firebase.firestore().collection("Garage")
+    .where("Lng", ">", mapCenter.lng() - 0.035)
+    .where("Lng", "<", mapCenter.lng() + 0.035)
+    .where("Lat", ">", mapCenter.lat() - 0.035)
+    .where("Lat", "<", mapCenter.lat() + 0.035)
+    .orderBy("Lng")
+    .get()
     .then((querySnapshot) => {
         querySnapshot.forEach((doc) => {
             const data = doc.data()
             console.log(doc.id + " => " + data);
             garageList.push(data);
-            addGarageMarker(data);
+            addMapMarker(data);
         });
     })
     .catch((error) => {
@@ -64,21 +75,35 @@ function setDefaultCoordinates() {
     userLocation = [40.78343000, -73.96625000];
 }
 
-function addMapMarker() {
-    garageList.forEach((parkingGarage) => {
-        var marker = new google.maps.Marker({
-            position: parkingGarage.location,
-            map: map,
-            title: parkingGarage.name
-        });
-
-        var infoWindow = new google.maps.InfoWindow({
-            content: '<div><strong>' + parkingGarage.name + '</strong><br>' +
-                    'Some information about the garage</div>'
-        });
-
-        marker.addListener('click', function() {
-            infoWindow.open(map, marker);
-        });
+function addMapMarker(garageData) {
+    var marker = new google.maps.Marker({
+        position: new google.maps.LatLng(garageData.Lat, garageData.Lng),
+        map: map,
+        title: garageData.Name
     });
+
+    var infoWindow = new google.maps.InfoWindow({
+        content: '<div><strong>' + garageData.Name + '</strong><br>' +
+                'Some information about the garage</div>'
+    });
+
+    marker.addListener('click', function() {
+        if (openInfoWindow != null) {
+            openInfoWindow.close();
+        }
+        openInfoWindow = infoWindow;
+        infoWindow.open(map, marker);
+    });
+    markers.push(marker);
+}
+
+function setMapOnAll(map) {
+    for (let i = 0; i < markers.length; i++) {
+      markers[i].setMap(map);
+    }
+  }
+  
+function deleteMarkers() {
+    setMapOnAll(null);
+    markers = [];
 }
