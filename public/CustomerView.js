9ba7e13aba617351c8a61a7ec24698a818068001
George N. - Macbook <91430969+Gevoooo@users.noreply.github.com>
2024-04-06 17:34:38 -0400

    feat: select markers and cards at the same time

--- public/CustomerView.js
+++ public/CustomerView.js
@@ -1,5 +1,4 @@
-var userLocation = [40.76343000, -73.98625000];
-var areaCode = 10024;
+var startLocation = [40.76343000, -73.98625000];
 var map;
 var mapCenter;
 var garageList = [];
@@ -7,19 +6,19 @@ var markers = [];
 var openInfoWindow = null;
 
 function initMap() {
-    mapCenter = new google.maps.LatLng(userLocation[0], userLocation[1]);
+    mapCenter = new google.maps.LatLng(startLocation[0], startLocation[1]);
     map = new google.maps.Map(document.getElementById('map'), {
         zoom: 15,
         streetViewControl: false,
         mapTypeControl: false,
-        center: {lat: userLocation[0], lng: userLocation[1]}
+        center: mapCenter
     });
     removedPOI = [
         {featureType: "poi.business", stylers: [{ visibility: "off" }],},
         {elementType: "labels.icon", stylers: [{ visibility: "off" }],}
     ];
     map.setOptions({styles: removedPOI});
-    navigator.geolocation.getCurrentPosition(setCoordinates, setDefaultCoordinates);
+    navigator.geolocation.getCurrentPosition(setCoordinates);
     google.maps.event.addListener(map, "dragend", async function() {
         mapCenter = await this.getCenter();
         garageList = [];
@@ -42,8 +41,7 @@ async function fillGarageList() {
         querySnapshot.forEach((doc) => {
             const data = doc.data()
             garageList.push(data);
-            displayOneGarage(data)
-            addMapMarker(data);
+            addMapMarker(data, doc.id);
         });
     })
     .catch((error) => {
@@ -51,32 +49,18 @@ async function fillGarageList() {
     });
 }
 
-function setLocation() {
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(setCoordinates, setDefaultCoordinates)
-    }
+async function setCoordinates(position) {
+    const lat = position.coords.latitude;
+    const lng = position.coords.longitude;
+    const latlng = new google.maps.LatLng(lat, lng);
+    map.setCenter(latlng);
+    mapCenter = latlng;
+    garageList = [];
+    deleteMarkers();
+    await fillGarageList();
 }
 
-function setCoordinates(position) {
-    userLocation = [position.coords.latitude, position.coords.longitude];
-    map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 12,
-        streetViewControl: false,
-        mapTypeControl: false,
-        center: {lat: userLocation[0], lng: userLocation[1]}
-    });
-    removedPOI = [
-        {featureType: "poi.business", stylers: [{ visibility: "off" }],},
-        {elementType: "labels.icon", stylers: [{ visibility: "off" }],}
-    ];
-    map.setOptions({styles: removedPOI});
-}
-
-function setDefaultCoordinates() {
-    userLocation = [40.78343000, -73.96625000];
-}
-
-function addMapMarker(garageData) {
+function addMapMarker(garageData, garageID) {
     var marker = new google.maps.Marker({
         position: new google.maps.LatLng(garageData.Lat, garageData.Lng),
         map: map,
@@ -89,12 +73,10 @@ function addMapMarker(garageData) {
     });
 
     marker.addListener('click', function() {
-        if (openInfoWindow != null) {
-            openInfoWindow.close();
-        }
-        openInfoWindow = infoWindow;
-        infoWindow.open(map, marker);
+        selectGarageMarker(marker, infoWindow);
+        selectGarageCard(garageID);
     });
+    displayOneGarage(garageData, garageID, marker, infoWindow);
     markers.push(marker);
 }
 
@@ -109,7 +91,7 @@ function deleteMarkers() {
     markers = [];
 }
 
-function displayOneGarage(data, garageID) {
+function displayOneGarage(data, garageID, marker, infoWindow) {
     let garageList = document.getElementById('GarageList');
     var newGarage = document.createElement('li');
     newGarage.className = 'bg-slate-300 p-3 mb-3 rounded-xl hover:bg-slate-400';
@@ -122,11 +104,37 @@ function displayOneGarage(data, garageID) {
     newGarage.id = garageID;
     newGarage.appendChild(pName);
     newGarage.appendChild(pAddress);
-    newGarage.onclick = function() {focusGarage(data.id)};
+    newGarage.onclick = function() {
+        selectGarageMarker(marker, infoWindow);
+        selectGarageCard(garageID);
+    };
     garageList.appendChild(newGarage);
 }
 
 function deleteGarageCards() {
     let garageList = document.getElementById('GarageList');
     garageList.innerHTML = "";
+}
+
+function selectGarageCard(garageID) {
+    let garageList = document.getElementById("GarageList");
+    for (var child of garageList.children) {
+        child.classList.remove("bg-black");
+        child.classList.add("bg-slate-300");
+    }
+    let card = document.getElementById(garageID);
+    card.classList.remove("bg-slate-300");
+    card.classList.add("bg-black");
+}
+
+function selectGarageMarker(marker, infoWindow) {
+    if (openInfoWindow != null) {
+        openInfoWindow.close();
+    }
+    openInfoWindow = infoWindow;
+    infoWindow.open(map, marker);
+}
+
+function handleBookButton() {
+
 }
