5c8ebba4cd868667976c3eefe55865a1c6d16015
TheftJord <voltstar78@gmail.com>
2024-04-19 00:35:52 -0400

    added two methods
    I have added saveVehicleChanges() and deleteVehicles() but they need to have the HTML added to it

--- public/SettingsPage.js
+++ public/SettingsPage.js
@@ -69,7 +69,53 @@ async function addVehicles(FuelTypeRef,LicensePlateRef,MakeRef,ModelRef,SizeRef,
  * @param {*} VehicleRef 
  */
 async function saveVehicleChanges(VehicleRef){
-
+    //variables
+    var fuelType,licensePlate,make,model,size,year;
+    //link db
+    const user=firebase.auth().currentUser,db=firebase.firestore();
+    //assign specific collection
+    const vehicleDB=db.collection("Vehicle").doc(VehicleRef);
+    //get info from HTML
+    /* will keep Ref var for testing will delete afterwards */
+    fuelType=/* document.getElementById("").value */FuelTypeRef;
+    licensePlate=/* document.getElementById("").value */LicensePlateRef;
+    make=/* document.getElementById("").value */MakeRef;
+    model=/* document.getElementById("").value */ModelRef;
+    size=/* document.getElementById("").value */SizeRef;
+    year=/* document.getElementById("").value  */YearRef;
+    //catches errors
+    if(inputNullorEmpty(fuelType)){
+        errorField.innerHTML="Please enter the fuel type";
+    }
+    else if(inputNullorEmpty(licensePlate)){
+        errorField.innerHTML="Please enter the license plate number";
+    }
+    else if(inputNullorEmpty(make)){
+        errorField.innerHTML="Please enter the make of the car";
+    }
+    else if(inputNullorEmpty(model)){
+        errorField.innerHTML="Please enter the model of the car";
+    }
+    else if(inputNullorEmpty(size)){
+        errorField.innerHTML="Please enter the size of the car";
+    }
+    else if(inputNullorEmpty(year)){
+        errorField.innerHTML="Please enter the make year of the car";
+    }
+    else if(!db.collection("Account").Type_ID.equals("Customer")){
+        errorField.innerHTML="You are not a customer please switch your account before contining";
+    }
+    //adds it to a doc
+    var vehicleDoc={
+        FuelType: fuelType,
+        LicensePlate: licensePlate,
+        Make: make,
+        Model: model,
+        Size: size,
+        Year: year
+    };
+    //updates database by merging the docs
+    await vehicleDB.set(vehicleDoc,{merge:true});
 }
 
 /**
@@ -78,7 +124,35 @@ async function saveVehicleChanges(VehicleRef){
  * @param {*} VehicleRef 
  */
 async function deleteVehicle(VehicleRef){
-
+    //variables
+    var customerID,accountType;
+    var vehicleLink="Vehicle/"+VehicleRef;
+    //link db
+    const user=firebase.auth().currentUser,db=firebase.firestore();
+    //gets customer doc id
+    const userAccount=await db.collection("Account").doc(user);
+    customerID=userAccount.Profile.slice(9);
+    accountType=userAccount.Type_ID;
+    //error check
+    if(!Type_ID.equals("Customer")){
+        errorField.innerHTML="You are not a customer please switch your account before contining";
+    }
+    else if(inputNullorEmpty(VehicleRef)){
+        errorField.innerHTML="Ngl I don't even know how the fuck you go here";
+    }
+    else if(inputNullorEmpty(customerID)||inputNullorEmpty(accountType)){
+        errorField.innerHTML="An error has occured while trying to access the database please contact an Admin as soon as possible";
+    }
+    //deletes from customer array
+    await db.collection("Customer").doc(customerID)
+    .update({
+        Garages: firebase.firestore.FieldValue.arrayRemove(vehicleLink)
+    });
+    //deletes doc from database
+    await db.collection("Vehicle").doc(VehicleRef).delete();
+    //updates HTML code
+    document.getElementById(VehicleRef).remove();
+    closePopup("");
 }
 
 
